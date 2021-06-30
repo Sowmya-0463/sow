6a0bc6f1f2b0dada1617e86026a1a63d403d49be
Sowmya-0463 <86717084+Sowmya-0463@users.noreply.github.com>
2021-06-30 18:41:13 +0530

    Add files via upload

---
+++ day2assignment.py
@@ -0,0 +1,34 @@
+Python 3.9.4 (tags/v3.9.4:1f2e308, Apr  6 2021, 13:40:21) [MSC v.1928 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> int(202.2)
+202
+>>> int(300)
+300
+>>> float(124.5)
+124.5
+>>> float("123")
+123.0
+>>> int(eat)
+Traceback (most recent call last):
+  File "<pyshell#4>", line 1, in <module>
+    int(eat)
+NameError: name 'eat' is not defined
+>>> float(xyz)
+Traceback (most recent call last):
+  File "<pyshell#5>", line 1, in <module>
+    float(xyz)
+NameError: name 'xyz' is not defined
+>>> str("mine")
+'mine'
+>>> str(1223)
+'1223'
+>>> str("123")
+'123'
+>>> str(1343.78)
+'1343.78'
+>>> str(virat)
+Traceback (most recent call last):
+  File "<pyshell#10>", line 1, in <module>
+    str(virat)
+NameError: name 'virat' is not defined
+>>> 
